6799e309ee706f50c921e6ed222ec4de04841cb9
Parth Purvesh <parth29dec@gmail.com>
2023-10-10 13:17:11 +0530

    Tests passed

--- src/main/java/ResistorColor.java
+++ src/main/java/ResistorColor.java
@@ -1,9 +1,11 @@
 class ResistorColor {
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return java.util.Arrays.asList(colors()).indexOf(color);
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
     }
 }
